2f0cae7e00182f945ee7f3603a7c8fb735cde8b2
Noweli <patryklewonn@gmail.com>
2019-04-09 00:32:14 +0200

    More precise, URL is now variable, removed unused constructor

--- src/logic/DataPrinter.java
+++ src/logic/DataPrinter.java
@@ -9,9 +9,11 @@ public class DataPrinter extends JPanel implements Runnable {
     public static final int WINDOW_WIDTH = 600;
     public static final int WINDOW_HEIGHT = 600;
 
-    private int lineX = 45;
+    private int lineX = 75;
     private double price;
-    private double oldPrice;
+
+    private int newX;
+    private int oldX = 0;
 
 
     private DataReceiver data;
@@ -38,23 +40,23 @@ public class DataPrinter extends JPanel implements Runnable {
     @Override
     public void paint(Graphics g) {
         g.setColor(Color.BLACK);
-        g.drawLine(40, 10, 40, WINDOW_HEIGHT - 60);
-        g.drawLine(40, WINDOW_HEIGHT - 60, WINDOW_WIDTH - 30, WINDOW_HEIGHT - 60);
+        g.drawLine(70, 10, 70, WINDOW_HEIGHT - 60);
+        g.drawLine(70, WINDOW_HEIGHT - 60, WINDOW_WIDTH - 30, WINDOW_HEIGHT - 60);
 
-        for (int i = WINDOW_HEIGHT - 70; i > 20; i -= 20) {
+        for (int i = WINDOW_HEIGHT - 100; i > 20; i -= 50) {
             g.drawString("$" + (WINDOW_HEIGHT - 50 - i), 0, i);
         }
 
         try {
-            if (price > 0)
-                oldPrice = price;
-            else
-                oldPrice = Double.parseDouble(data.getLine().substring(1));
-
-
             price = Double.parseDouble(data.getLine().substring(1));
             System.out.println(price);
-            g.drawLine(lineX, (int) oldPrice, lineX - 5, (int) price);
+            newX = (int)((WINDOW_HEIGHT - 100) - price/1.1627);
+
+            if(oldX == 0) oldX = newX;
+
+            System.out.println(newX);
+            g.drawLine(lineX, oldX, lineX - 5, newX);
+            oldX = newX;
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/logic/DataReceiver.java
+++ src/logic/DataReceiver.java
@@ -7,22 +7,18 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public class DataReceiver {
+    private String url = "https://www.nasdaq.com/symbol/tsla/real-time";
+
     private URL dataURL;
     private URLConnection connection;
     private InputStreamReader inStream;
     private BufferedReader reader;
 
     public DataReceiver() throws IOException {
-        dataURL = new URL("https://www.nasdaq.com/symbol/tsla/real-time");
+        dataURL = new URL(url);
 
     }
-
-    public DataReceiver(String URL) throws IOException {
-        dataURL = new URL(URL);
-        connection = dataURL.openConnection();
-        inStream = new InputStreamReader(connection.getInputStream());
-        reader = new BufferedReader(inStream);
-    }
+    
 
     public String getLine() throws IOException {
         String line = null;
@@ -33,11 +29,11 @@ public class DataReceiver {
         reader.mark(60000);
 
         line = reader.readLine();
-        while(line != null){
-            if(line.contains("class=\"qwidget-dollar\">")){
+        while (line != null) {
+            if (line.contains("class=\"qwidget-dollar\">")) {
                 int start = line.indexOf(">$");
-                if(start > 0 ){
-                    line = line.substring(start+1, start + 8);
+                if (start > 0) {
+                    line = line.substring(start + 1, start + 8);
                     reader.reset();
                     return line;
                 }
